b8b54007ac907fd7bd192fe7ae653dbc2949c6ab
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-04-22 23:01:45 -0400

    Update app_runner.py

--- src/tempo_core/app_runner.py
+++ src/tempo_core/app_runner.py
@@ -2,41 +2,44 @@ from __future__ import annotations
 
 import os
 import subprocess
+from pathlib import Path
+from _collections_abc import Sequence
 
 from tempo_core import file_io, logger
 from tempo_core.data_structures import ExecutionMode
 import tempo_core.settings
 
+default_working_dir = tempo_core.settings.get_temp_directory()
 
 def run_app(
-    exe_path: str,
+    exe_path: Path,
     exec_mode: ExecutionMode = ExecutionMode.SYNC,
-    args: list[str] | None = None,
-    temp_dir: str = tempo_core.settings.get_temp_directory(),
+    args: Sequence[str | Path] | None = None,
+    working_dir: Path = default_working_dir,
 ) -> None:
-    os.makedirs(temp_dir, exist_ok=True)
+    working_dir.mkdir(parents=True, exist_ok=True)
 
     if not args:
         args = []
-    exe_path = file_io.ensure_path_quoted(exe_path)
+    exe_path_str = file_io.ensure_path_quoted(str(exe_path))
 
     if exec_mode == ExecutionMode.SYNC:
-        command = exe_path
+        command = exe_path_str
         for arg in args:
             command = f"{command} {arg}"
         logger.log_message("----------------------------------------------------")
-        logger.log_message(f"Command: main executable: {exe_path}")
+        logger.log_message(f"Command: main executable: {exe_path_str}")
         for arg in args:
             logger.log_message(f"Command: arg: {arg}")
         logger.log_message("----------------------------------------------------")
         logger.log_message(f"Command: {command} running with the {exec_mode} enum")
-        os.makedirs(temp_dir, exist_ok=True)
-        if temp_dir and os.path.isdir(temp_dir):
-            os.chdir(temp_dir)
+
+        if working_dir and working_dir.is_dir():
+            os.chdir(working_dir)
 
         process = subprocess.Popen(
             command,
-            cwd=temp_dir,
+            cwd=working_dir,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             text=True,
@@ -53,8 +56,8 @@ def run_app(
         logger.log_message(f"Command: {command} finished")
 
     elif exec_mode == ExecutionMode.ASYNC:
-        command = exe_path
+        command = exe_path_str
         for arg in args:
             command = f"{command} {arg}"
         logger.log_message(f"Command: {command} started with the {exec_mode} enum")
-        subprocess.Popen(command, cwd=temp_dir, start_new_session=True)
+        subprocess.Popen(command, cwd=working_dir, start_new_session=True)
