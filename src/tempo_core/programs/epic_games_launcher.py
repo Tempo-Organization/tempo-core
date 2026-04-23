d7e33f46dcd2332cb9565da79145248a50516de1
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-04-22 22:00:15 -0400

    Update epic_games_launcher.py

--- src/tempo_core/programs/epic_games_launcher.py
+++ src/tempo_core/programs/epic_games_launcher.py
@@ -1,11 +1,12 @@
 import os
 import sys
+from pathlib import Path
 
 if sys.platform == "win32":
     import winreg
 
 
-def get_epic_launcher_exe_location() -> str:
+def get_epic_launcher_exe_location() -> Path:
     if sys.platform != "win32":
         invalid_os_error = (
             "Epic Games Launcher path retrieval is only supported on Windows."
@@ -15,7 +16,7 @@ def get_epic_launcher_exe_location() -> str:
     first_arg = winreg.HKEY_LOCAL_MACHINE
     second_arg = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
     reg_key = winreg.OpenKey(
-        first_arg, second_arg, 0, winreg.KEY_READ | winreg.KEY_WOW64_32KEY
+        first_arg, second_arg, 0, winreg.KEY_READ | winreg.KEY_WOW64_32KEY,
     )
 
     for i in range(winreg.QueryInfoKey(reg_key)[0]):
@@ -25,9 +26,7 @@ def get_epic_launcher_exe_location() -> str:
             display_name = winreg.QueryValueEx(sub_key, "DisplayName")[0]
             if "Epic Games Launcher" in display_name:
                 install_path, _ = winreg.QueryValueEx(sub_key, "InstallLocation")
-                return os.path.join(
-                    install_path, "Portal", "Binaries", "Win32", "EpicGamesLauncher.exe"
-                )
+                return Path(install_path / "Portal" / "Binaries" / "Win32" / "EpicGamesLauncher.exe")
         except FileNotFoundError:
             pass
         finally:
