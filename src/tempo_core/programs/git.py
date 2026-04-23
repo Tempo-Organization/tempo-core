ce18157a76d2523da9e39b502d2769fba76d0437
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-04-22 21:51:38 -0400

    Update git.py

--- src/tempo_core/programs/git.py
+++ src/tempo_core/programs/git.py
@@ -1,28 +1,32 @@
 import os
 import requests
+from pathlib import Path
 
 from tempo_core import logger, online_check
 
+default_current_dir = Path.cwd()
 
 def download_files_from_github_repo(
     repo_url: str,
     repo_branch: str = "master",
-    file_paths: list[str] = [],
-    output_directory: str = os.getcwd(),
+    file_paths: list[str] | None = None,
+    output_directory: Path = default_current_dir,
 ) -> None:
+    if not file_paths or len(file_paths) == 0:
+        return
     if not online_check.is_online:
         raise RuntimeError('You are not able to download files from github repos when not connected to the web.')
     try:
         parts = repo_url.strip("/").split("/")
         user, repo = parts[-2], parts[-1]
-    except IndexError:
-        raise ValueError("Invalid GitHub repository URL")
+    except IndexError as err:
+        raise ValueError("Invalid GitHub repository URL") from err
 
     for file_path in file_paths:
         raw_url = (
             f"https://raw.githubusercontent.com/{user}/{repo}/{repo_branch}/{file_path}"
         )
-        local_file_path = os.path.join(output_directory, file_path)
+        local_file_path = output_directory / file_path
 
         try:
             response = requests.get(raw_url)
@@ -31,7 +35,7 @@ def download_files_from_github_repo(
             logger.log_message(f"Failed to download {file_path}: {e}")
             continue
 
-        os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
-        with open(local_file_path, "wb") as f:
+        local_file_path.parent.mkdir(parents=True, exist_ok=True)
+        with local_file_path.open("wb") as f:
             f.write(response.content)
             logger.log_message(f"Downloaded: {file_path} to {local_file_path}")
