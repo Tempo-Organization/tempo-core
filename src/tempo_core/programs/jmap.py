4b0fc7929bc95483a067fa1fa221c8b44607db41
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-04-22 21:52:04 -0400

    Update jmap.py

--- src/tempo_core/programs/jmap.py
+++ src/tempo_core/programs/jmap.py
@@ -1,33 +1,31 @@
 import os
-import pathlib
+from pathlib import Path
 
 from tempo_core import settings, app_runner, data_structures
 
 
 def run_dump_jmap_jmap_command(
-    jmap_executable: str,
+    jmap_executable: Path,
     game_pid: int,
-    output_jmap_location: pathlib.Path
+    output_jmap_location: Path,
 ) -> None:
     unreal_engine_dir = settings.get_unreal_engine_dir()
     if not unreal_engine_dir:
         raise RuntimeError('no unreal engine dir')
-    unreal_engine_dir = str(unreal_engine_dir)
     unreal_engine_version = settings.get_unreal_engine_version(unreal_engine_dir)
     if not unreal_engine_version:
         raise RuntimeError('no unreal engine version')
     engine_ver_string = unreal_engine_version.get_jmap_unreal_version_str()
     os.environ["PATTERNSLEUTH_RES_EngineVersion"] = engine_ver_string
-    exe_path = os.path.normpath(str(jmap_executable))
     exec_mode = data_structures.ExecutionMode.SYNC
     args = [
         '--pid',
         game_pid,
-        output_jmap_location
+        output_jmap_location,
     ]
 
     app_runner.run_app(
-        exe_path=exe_path,
+        exe_path=jmap_executable,
         exec_mode=exec_mode,
-        args=args # ty: ignore
+        args=args, # ty: ignore
     )
