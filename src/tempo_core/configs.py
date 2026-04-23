111119d0b1df28918e615e8395b6e552f808cb16
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-04-22 21:32:17 -0400

    Update configs.py

--- src/tempo_core/configs.py
+++ src/tempo_core/configs.py
@@ -1,42 +1,42 @@
-import os
-from typing import Protocol, TypeAlias
+# import os
+# from typing import Protocol, TypeAlias
 
 
-JSONValue: TypeAlias = (
-    str | int | float | bool | None | list["JSONValue"] | dict[str, "JSONValue"]
-)
+# JSONValue: TypeAlias = (
+#     str | int | float | bool | None | list["JSONValue"] | dict[str, "JSONValue"]
+# )
 
 
-def resolve_special_vars(value: JSONValue) -> JSONValue:
-    special_vars: dict[str, str] = {
-        "${workspaceFolder}": os.path.abspath(
-            os.path.dirname(__file__)
-        ),
-        "${home}": os.path.expanduser("~"),
-        "${cwd}": os.getcwd(),
-    }
+# def resolve_special_vars(value: JSONValue) -> JSONValue:
+#     special_vars: dict[str, str] = {
+#         "${workspaceFolder}": os.path.abspath(
+#             os.path.dirname(__file__),
+#         ),
+#         "${home}": os.path.expanduser("~"),
+#         "${cwd}": os.getcwd(),
+#     }
 
-    if isinstance(value, str):
-        for key, replacement in special_vars.items():
-            value = value.replace(key, replacement)
-    return value
-
-
-class SupportsGetAndAttr(Protocol):
-    def get(
-        self, key: str, default: JSONValue | None = None
-    ) -> JSONValue | None: ...
-    def __getattr__(self, name: str) -> JSONValue: ...
+#     if isinstance(value, str):
+#         for key, replacement in special_vars.items():
+#             value = value.replace(key, replacement)
+#     return value
 
 
-class DynamicSettings:
-    def __init__(self, settings: SupportsGetAndAttr) -> None:
-        self._settings = settings
+# class SupportsGetAndAttr(Protocol):
+#     def get(
+#         self, key: str, default: JSONValue | None = None,
+#     ) -> JSONValue | None: ...
+#     def __getattr__(self, name: str) -> JSONValue: ...
 
-    def __getattr__(self, item: str) -> JSONValue:
-        value = getattr(self._settings, item, None)
-        return resolve_special_vars(value)
 
-    def __getitem__(self, item: str) -> JSONValue | None:
-        value = self._settings.get(item)
-        return resolve_special_vars(value)
+# class DynamicSettings:
+#     def __init__(self, settings: SupportsGetAndAttr) -> None:
+#         self._settings = settings
+
+#     def __getattr__(self, item: str) -> JSONValue:
+#         value = getattr(self._settings, item, None)
+#         return resolve_special_vars(value)
+
+#     def __getitem__(self, item: str) -> JSONValue | None:
+#         value = self._settings.get(item)
+#         return resolve_special_vars(value)
